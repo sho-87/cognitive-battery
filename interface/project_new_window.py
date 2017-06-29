8d8506115f9e51b399d23fab2c6e065382c788a3
Simon Ho <simonsays87@googlemail.com>
2017-06-29 09:17:13 -0700

    Create new project window

---
+++ interface/project_new_window.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from designer import project_new_window_qt
+
+
+class NewProjectWindow(QtWidgets.QDialog, project_new_window_qt.Ui_NewProjectWindow):
+    def __init__(self):
+        super(NewProjectWindow, self).__init__()
+
+        # Setup the main window UI
+        self.setupUi(self)
+
+        # Set app icon
+        self.setWindowIcon(QtGui.QIcon(os.path.join('images', 'icon_sml.png')))
+
+        # Delete the object when dialog is closed
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+
+        # Remove the help / whats this button from title bar
+        self.setWindowFlags(
+            self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+
+        # Bind button events
+        self.dirSelectButton.clicked.connect(self.select_file)
+        self.createButton.clicked.connect(self.close)
+        self.cancelButton.clicked.connect(self.close)
+
+    def select_file(self):
+        self.file_dialog = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select directory for the project')
+        self.dirValue.setText(self.file_dialog)
--- interface/project_window.py
+++ interface/project_window.py
@@ -3,7 +3,7 @@ import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from designer import project_window_qt
-from interface import battery_window
+from interface import battery_window, project_new_window
 
 
 class ProjectWindow(QtWidgets.QMainWindow, project_window_qt.Ui_ProjectWindow):
@@ -16,8 +16,9 @@ class ProjectWindow(QtWidgets.QMainWindow, project_window_qt.Ui_ProjectWindow):
         # Set app icon
         self.setWindowIcon(QtGui.QIcon(os.path.join('images', 'icon_sml.png')))
 
-        # Keep reference to main battery window
+        # Keep reference to main battery and new project windows
         self.main_battery = None
+        self.new_project_window = None
 
         self.res_width = res_width
         self.res_height = res_height
@@ -25,11 +26,16 @@ class ProjectWindow(QtWidgets.QMainWindow, project_window_qt.Ui_ProjectWindow):
         self.first_run = first_run
 
         # Handle menu bar item click events
+        self.actionNewProject.triggered.connect(self.new_project)
         self.actionExit.triggered.connect(self.close)
 
         # Bind button events
         self.startButton.clicked.connect(self.start)
         self.closeButton.clicked.connect(self.close)
+
+    def new_project(self):
+        self.new_project_window = project_new_window.NewProjectWindow()
+        self.new_project_window.show()
 
     def start(self, event):
         self.main_battery = battery_window.BatteryWindow(self.directory,
